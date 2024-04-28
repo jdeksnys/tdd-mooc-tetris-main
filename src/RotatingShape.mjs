8de823136578c1d8787565b283ed0a0e280e6a7c
jdeksnys <jonasdeksnys@gamil.com>
2024-04-28 15:07:05 +0300

    test-pass

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,11 +1,19 @@
 
 export class RotatingShape{
     shape = "";
+    rows = 0;
+    cols = 0;
     
     constructor(str){
+        let cols = 0;
         for(let i=0; i<str.length; i++){
             if(str[i] != '\t' && str[i] != ' '){
+                if(str[i] == '\n'){
+                    this.cols = cols;
+                    this.rows += 1;
+                }
                 this.shape += str[i];
+                cols += 1;
             }
         }
     }

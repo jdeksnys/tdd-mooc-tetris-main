5c6c82be135c8373b8d2f2665ff5f692f6ae2591
jdeksnys <jonasdeksnys@gamil.com>
2024-04-29 23:55:08 +0300

    test-pass

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -7,11 +7,19 @@ export class Tetromino {
     static T_SHAPE = new RotatingShape(
         `.T.
          TTT
-         ...`);
+         ...`
+    );
+
     static I_SHAPE = new RotatingShape(
         `.....
          .....
          IIII.
          .....
-         .....`);
+         .....`
+    );
+    
+    static O_SHAPE = new RotatingShape(
+        `.OO
+        .OO
+        ...`);
 }

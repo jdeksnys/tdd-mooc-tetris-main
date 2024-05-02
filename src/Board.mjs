e12ac752a05f37386b4d4e7b59532790540add90
jdeksnys <jonasdeksnys@gamil.com>
2024-05-02 13:55:04 +0300

    test-pass

--- src/Board.mjs
+++ src/Board.mjs
@@ -88,6 +88,9 @@ export class Board {
   get_L_most_coord(){
     for(let i=0; i<this.fallBlock.shape.rows; i++){
       let r_started = false;
+      for(let j=this.fallBlock.x_pos+this.fallBlock.cols-1; j>=0; j--){
+        
+      }
     }
 
 

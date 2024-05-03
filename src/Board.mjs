d00b6eaf6aacd1cb2f87550f2ee858a914259c56
jdeksnys <jonasdeksnys@gamil.com>
2024-05-04 00:01:18 +0300

    test-pass

--- src/Board.mjs
+++ src/Board.mjs
@@ -323,12 +323,16 @@ export class Board {
     } else if(this.can_wall_kick()){
       let rot_shape = new RotatingShape(this.fallBlock.shape.toString());
       this.updateFallblockInBoard(true);
-      let dist_L = this.get_dist_to_wall_L();
-      let dist_R = this.get_dist_to_wall_R();
-      let actual_cols = this.get_shape_actual_cols();
-      let actual_rows = this.get_shape_actual_rows();
+      // let dist_L = this.get_dist_to_wall_L();
+      // let dist_R = this.get_dist_to_wall_R();
+      // let actual_cols = this.get_shape_actual_cols();
+      // let actual_rows = this.get_shape_actual_rows();
       let new_x = 0;
       let new_y = 0;
+      
+      // if(dist_L + actual_cols < actual_rows){
+      //   new_x = 
+      // }
       // this.fallBlock = new FallingBlock(rot_shape.rotateRight(), this.width, this.height, this.fallBlock.x_pos, this.fallBlock.y_pos);
       this.updateFallblockInBoard(false);
 

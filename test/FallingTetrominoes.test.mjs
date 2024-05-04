f6cf4dd8fd4a7685cc4a3ff797d4fe58f21af0a8
jdeksnys <jonasdeksnys@gamil.com>
2024-05-04 16:46:36 +0300

    test-pass

--- test/FallingTetrominoes.test.mjs
+++ test/FallingTetrominoes.test.mjs
@@ -1,9 +1,7 @@
 
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
-import { Board } from "../src/Board.mjs";
 import { Board2 } from "../src/Board2.mjs";
-import { Tetromino } from "../src/Tetromino.mjs";
 import { Tetromino2 } from "../src/Tetromino2.mjs";
 
 function fallToBottom(board) {
@@ -13,10 +11,8 @@ function fallToBottom(board) {
 }
 
 describe("Falling tetrominoes", () => {
-  let board;
   let board2;
   beforeEach(() => {
-    board = new Board(10, 6);
     board2 = new Board2(10, 6);
   });
 

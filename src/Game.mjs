733f882faaf598c47808749d5cf7abcbea04440e
jdeksnys <jonasdeksnys@gamil.com>
2024-05-06 22:57:54 +0300

    test-pass

--- src/Game.mjs
+++ src/Game.mjs
@@ -4,5 +4,10 @@ export class Game{
     tickDuration;
     nextTick;
 
-    constructor(columns, rows, tickDuration, nextTick){}
+    constructor(columns, rows, tickDuration, nextTick){
+        this.columns = columns;
+        this.rows = rows;
+        this.tickDuration = tickDuration;
+        this.nextTick = nextTick;
+    }
 }

1b991a7fa67322a259d578d323936b1b5dd90eca
jdeksnys <jonasdeksnys@gamil.com>
2024-05-06 23:05:31 +0300

    test-pass

--- src/ScoringSystem.mjs
+++ src/ScoringSystem.mjs
@@ -6,7 +6,13 @@ export class ScoringSystem {
 
     linesCleared(lines_cleared){
         if(lines_cleared == 1){
-
+            this.tot_points += 40;
+        } else if(lines_cleared == 2){
+            this.tot_points += 100;
+        } else if(lines_cleared == 3){
+            this.tot_points += 300;
+        } else if(lines_cleared == 4){
+            this.tot_points += 1200;
         }
     }
 }
